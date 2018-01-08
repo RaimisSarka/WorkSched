fab3f390d85e94b7f0aed97920445cdbbe0abc1d
RaimisSarka <raimis.sarka@gmail.com>
2018-01-08 04:56:15 +0200

    Added date picker to AddShiftActivity

--- app/src/main/java/com/raimissarka/worksched/AddShiftActivity.java
+++ app/src/main/java/com/raimissarka/worksched/AddShiftActivity.java
@@ -1,9 +1,19 @@
 package com.raimissarka.worksched;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.Calendar;
 
 public class AddShiftActivity extends AppCompatActivity {
 
@@ -21,5 +31,40 @@ public class AddShiftActivity extends AppCompatActivity {
 
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
+
+        EditText mShiftStartDate = (EditText) findViewById(R.id.et_shift_start_date_selector_value);
+
+        ImageView mSetDateButton = (ImageView) findViewById(R.id.iw_date_pick_button);
+
+        mSetDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+            }
+        });
     }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            
+        }
+
+    }
+
 }
